e71eefc8a073ca72026a9b1bf400a119b10b6930
Robert de Lorimier <r_delorimier@zoho.com>
2021-01-25 21:31:04 -0700

    Master added quite a few updates; Wow codebook knitting is a pain

--- README.md
+++ README.md
@@ -1,43 +1,73 @@
 ---
-title: "CodeBook"
+title: "README"
 author: "Robert de Lorimier"
 date: "1/16/2021"
 output: html_document
 ---
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+## Overview
 
-## R Markdown
+These data for this project is retrieved (see reference "Source Data Set") and contains data related to accelerometer experiments. The experiments were carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope. The original data captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. See the README.txt, for more details of the original data, found by default at "data/testData/UCI HAR Dataset" by the default after running the function to create the data. The folder, "sample_data" should contain the same set and is included in the project.
 
-One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+The data has been unified for the "tidy" data set, and only contains standard deviation and means values for the measurements for the first file, accelerometer_data.csv, and the mean of each of those measurements aggregated by student and activity in file, accelerometer_activity_subject_means.csv.
 
-http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+## Requirement
 
-Here are the data for the project:
+RStudio is required to re-Knit codebook html files. If you need to recreate the codebooks, please install RStudio.
 
- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  
+## References
 
-This project includes an R script called run_analysis.R that does the following. 
+- Source Data Set: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+- Explanation of Data: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
 
-1. Merges the training and the test sets to create one data set.
-1. Extracts only the measurements on the mean and standard deviation for each measurement. 
-1. Uses descriptive activity names to name the activities in the data set
-1. Appropriately labels the data set with descriptive variable names. 
-1. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Tidy Data Set Replication Steps
+
+This project includes an R script called run_analysis.R that is a script used to create the tidy datasets. To run, do these steps:
+
+1.  From the project folder in the R command prompt, run 
+  + ```source("run_analysis.R")```
+2.  From the command prompt then run 
+  + ```create_tidy_data()```
+
+The function will perform these operations:
+
+1. Download and expand the data from the website into the folder "data" by default, and creates a folder under it, "UCI HAR Dataset", with the data files.
+2.  For each folder, "train" or "test" noted as ***```folder_name```***, these steps are performed:
+    a.  Retrieve the main set of measurement data at ```folder_name```/X_```folder_name```.txt
+    b.  Add the subject column from subject_```folder_name```/```folder_name```.txt to the main set.
+    c.  Add the activity names in a column using ```folder_name```/y_```folder_name```.txt to look up the activity label in activity_labels.txt
+    d.  Add the values in features.txt as the column headers, and modifying the names to more readable ones
+    e.  Filter out only columns with standard deviation or means values.
+3.  The two data sets are then unioned to create a single set of standard deviation and means values for the students and activities.
+4.  A second data set is then also created, grouping by student and activity, with the mean of each measurement column.
+5.  Two files are then created under the folder, "tidy_date":
+  - accelerometer_data.csv for the full data set
+  - accelerometer_activity_subject_means.csv, for the grouped data set by student and activity
 
 
-```{r cars}
-summary(cars)
-```
+## Codebook Creation Replication Steps
 
-## Including Plots
+To create a new set of codebooks for the any newly created tidy set, these steps will need to be taken
 
-You can also embed plots, for example:
+1. From the RStudio command prompt in the project folder run commands to install the required libraries
+   + install.packages("codebook")
+   + install.packages("rio")
+   + install.packages("future")
+   + install.packages("labelled")
+   + install.packages("dplyr")
+   + install.packages("data.table")
+2. Run the commands to create the codebook sources
+   + ```create_accelerometer_codebook()```
+   + ```create_accelerometer_grouped_codebook```
+3. From RStudio open each codebook markdown file, AccelerometerDataCodebook.Rmd and AccelerometerGroupedDataCodebook.Rmd, and choose Knit from the tab menu. This will create files AccelerometerDataCodebook.html and AccelerometerGroupedDataCodebook.html
 
-```{r pressure, echo=FALSE}
-plot(pressure)
-```
 
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+## License:
+
+This original data set was used in this publication:
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,273 @@
-dd
+#' Script to package data for wearable computing measurements into a tidy dataset.
+#' 
+#' @description 
+#' This class has 9 functions:
+#' - create_tidy_data: The top level function, it pulls a set of data from a 
+#' remote http(s) location and creates the tidy data set files in a new 
+#' tidy_datasets folder.
+#' - create_student_activity_grouped_mean_df: Take the main unaggregated set and 
+#' aggregates the data by student and activity, with the mean of all measurement columns
+#' - prepend_column_name_with_groupedmeans: Prepends the column name with 
+#' "grouped_studentactivity_mean_of_"
+#' - get_packaged_data_set: This function retrieves data from either of the 
+#' folders "train" or "test", 
+#' and returns a formatted data table with results, subject, and activity data,
+#' formatted with appropriate headers
+#' - get_subject_id_df: Retreives the subject id data from the appropriate 
+#' folder and returns as a data.table
+#' - get_activity_df: Retreives the activtiy id data from the parent folder and 
+#' returns as a data.table
+#' - get_result_set_df: Retreives the results data from the appropriate folder 
+#' and returns as a data.table
+#' - get_selected_column_names_and_indexes_df: Retreives the column header data 
+#' for the result set from the parent folder and returns a formatted subset as a 
+#' data.table
+#' - rename_column: A utilitiy function that alters the name of a column string 
+#' to a more readable form, used in get_selected_column_names_and_indexes_df
+
+
+#' This function retrieves data from the accelerometer website and creates two 
+#' tidy sets of data from it  for accelerometer data, one for the standard 
+#' deviation and means data, and another for the grouped mean of each selected 
+#' field, grouped by subject and activity.
+#' 
+#' @param data_dir A directory where the downloaded data will expanded, defaults
+#' to "data"
+#' @param dataZipUrl A url where the data zip file is located, defaults to 
+#' "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#' @param tidy_data_folder A folder location where the tidy data comma 
+#' delimited files are persisted.
+#' @return A list with two dataframes, the unaggregated data "result_set", and aggregated data tables "aggregated_set"
+#' @examples
+#' > create_tidy_data(data_dir = "data", data_zip_url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", tidy_data_folder = "tidy_datasets")
+#'
+#' @export
+create_tidy_data <- function(
+  data_dir = "data", 
+  data_zip_url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+  tidy_data_folder = "tidy_datasets"
+  ) {
+  print("START")
+  # LIBRARIES / DEPENDENCIES
+  
+#  install.packages("RCurl")
+  library(RCurl)
+#  install.packages("data.table")
+  library(data.table)
+#  install.packages("dplyr")
+  library(dplyr)
+  
+  # SET UP DATA LOCATIONS AND VARIABLES
+  print("Set up data location and variables")
+  unlink(data_dir, recursive = TRUE)
+  dir.create(data_dir)
+  downloadZipfileLocation <- paste(data_dir, "testData.zip", sep="/")
+  downloadExpandedFolderLocation <- paste(data_dir, "testData", sep="/")
+  dataUrl <- data_zip_url
+  print(paste("Download data to ", downloadZipfileLocation, " and expand to ", downloadExpandedFolderLocation, sep=""))
+  download.file(url=data_zip_url, downloadZipfileLocation, method='curl')
+  unzip(downloadZipfileLocation, exdir=downloadExpandedFolderLocation)
+  topRootFolder <- dir(downloadExpandedFolderLocation)[1]
+  root_path <- paste(downloadExpandedFolderLocation, topRootFolder, sep="/")
+  print(paste("Root folder is  ", root_path, sep=""))
+  
+  # EXTRACT AND PACKAGE DATA
+  
+  # CREATE ACTIVITY LOOKUP DATAFRAME
+  print("Create activity and lookup dataframe")
+  activity_lookup_df <- fread(paste(root_path, "activity_labels.txt", sep="/"))
+  names(activity_lookup_df) <- c("activity_id", "activity_name")
+  
+  # GET STANDARD DEVIATON AND MEANS C0LUMNS INDEXES AND CREATE HUMAN READABLE COLUMN NAMES
+  print("Create dataframe for results headers and indexes")
+  selected_column_names_indexes_df <- get_selected_column_names_and_indexes_df(root_path)
+
+  # GET EACH PACKAGED DATAFRAME FROM TRAIN AND TEST AND UNION THEM
+  print("Get formatted result sets from test and train folders")
+  testDF <- get_packaged_data_set(root_path, "test", selected_column_names_indexes_df, activity_lookup_df)
+  trainDF <- get_packaged_data_set(root_path, "train", selected_column_names_indexes_df, activity_lookup_df)
+  print("Create tidy set data tables")
+  fullDF <- rbind(testDF, trainDF)
+  aggDF <- create_student_activity_grouped_mean_df(fullDF)
+  
+  # Union the various test and train dataframes
+  print(paste("Write tidy sets to ", tidy_data_folder, sep=""))
+  unlink(tidy_data_folder, recursive = TRUE)
+  dir.create(tidy_data_folder)
+  fwrite(fullDF, file = paste(tidy_data_folder, "accelerometer_data.csv", sep="/"))
+  fwrite(aggDF, file = paste(tidy_data_folder, "accelerometer_activity_subject_means.csv", sep="/"))
+  print("END")
+  return_list <- list(result_set=fullDF, aggregated_set=aggDF)
+  return(return_list)
+}
+
+#' This function takes the main unaggregated set and aggregates the data by 
+#' student and activity, with the mean of all measurement columns. The column 
+#' header are prepended with indication that the values are aggregated means.
+#' 
+#' @param df The unaggregate accelerometer data table
+#' @examples
+#' > aggDF <- create_student_activity_grouped_mean_df(fullDF)
+#' @return An aggregated data table with altered columns
+#' @export
+create_student_activity_grouped_mean_df <- function(df) {
+  aggr_df = aggregate(x=df[,3:86], by=list(grouped_activity=df$activity_name, grouped_subject=df$subject_id), FUN=mean, na.rm=TRUE)
+  col_labels <- names(aggr_df)
+  altered_colnames <- lapply(col_labels, prepend_column_name_with_groupedmeans)
+  names(aggr_df) <- altered_colnames
+  return(aggr_df)
+}
+
+#' Prepends the column name with "grouped_studentactivity_mean_of_"
+#' 
+#' @param colName string of the column name
+#' @examples
+#' > newColName <- prepend_column_name_with_groupedmeans("timeof_body_accel_mean_x")
+#' > newColName
+#' [1] "grouped_studentactivity_mean_of_ timeof_body_accel_mean_x"
+#' @return A new string with appended value
+#' @export
+prepend_column_name_with_groupedmeans <- function(col_name) {
+  if(col_name == "grouped_subject" || col_name == "grouped_activity") {
+    return(col_name)
+  } else {
+    return(paste("grouped_studentactivity_mean_of_", col_name))
+  }
+}
+
+#' This function retrieves data from either of the folders "train" or "test", 
+#' and returns a formatted data table with results, subject, and activity data,
+#' formatted with appropriate headers
+#' 
+#' @param root_path The root path under which the train and test folders are located
+#' to "data"
+#' @param folder_type Either "test" or "train", the two folders with result data
+#' @param target_columns_df A data table with the column header labels and indices
+#' to select the appropriate columns
+#' @param activity_lookup_df A data table with the activity ids and label to add
+#' as a column to the result set data table
+#' @examples
+#' > get_packaged_data_set("/path/to/data/folder", "test", target_columns_df, activity_lookup_df)
+#' @return Formatted data table with headers, subject id, activity, results for 
+#' selected column for mean and standard deviation
+#' @export
+get_packaged_data_set <- function(root_path, folder_type, target_columns_df, activity_lookup_df) {
+  # CREATE SUBJECT ID DATAFRAME
+  subjectIdDF <- get_subject_id_df(root_path, folder_type)
+  # CREATE ACTION ID DATAFRAME
+  actionDF <- get_activity_df(root_path, folder_type, activity_lookup_df)
+  # CREATE RESULT DATAFRAME
+  resultDF <- get_result_set_df(root_path, folder_type, target_columns_df)
+  # CREATE PACKAGES DATAFRAME AND RETURN
+  returnDf <- cbind(subjectIdDF, actionDF)
+  returnDf <- cbind(returnDf, resultDF)
+  returnDf
+}
+
+#' This function retreives the subject id data from the appropriate folder and 
+#' returns as a data.table
+#' 
+#' @param root_path The root path under which the train and test folders are located
+#' to "data"
+#' @param folder_type Either "test" or "train", the two folders with result data
+#' @examples
+#' > get_subject_id_df("/path/to/data/folder", "test")
+#' @return Formatted data table with subject id column
+#' @export
+get_subject_id_df <- function(root_path, folder_type) {
+  subjectFileName <- paste("subject_", folder_type, ".txt", sep="")
+  subjectIdDF <- fread(paste(root_path, folder_type, subjectFileName, sep = "/"))
+  names(subjectIdDF) <- c("subject_id")
+  subjectIdDF
+}
+
+#' This function retreives the activtiy id data from a sub-folder and 
+#' returns as a data.table
+#' 
+#' @param root_path The root path under which the train and test folders are located
+#' to "data"
+#' @param folder_type Either "test" or "train", the two folders with result data
+#' @param activity_lookup_df data table with activity lookup ids to labels
+#' @examples
+#' > get_activity_df("/path/to/data/folder", "test", activity_lookup_df)
+#' @return Formatted activity column data table to join with results data table
+#' @export
+get_activity_df <- function(root_path, folder_type, activity_lookup_df) {
+  actionFileName <- paste("y_", folder_type, ".txt", sep="")
+  actionIdDF <- fread(paste(root_path, folder_type, actionFileName, sep = "/"))
+  names(actionIdDF) <- c("activity_id")
+  actionsLabelDf <- merge(actionIdDF, activity_lookup_df, by = "activity_id")
+  actionsLabelDf$activity_id = NULL
+  actionsLabelDf
+}
+
+#' This function retreives the result data from a sub-folder and returns as a data.table
+#' 
+#' @param root_path The root path under which the train and test folders are located
+#' to "data"
+#' @param folder_type Either "test" or "train", the two folders with result data
+#' @param target_columns_df data table headers and indexes to select columns
+#' @examples
+#' > get_result_set_df("/path/to/data/folder", "test", target_columns_df)
+#' @return Formatted result set data table with formatted column names and only
+#' standard deviation and mean columns
+#' @export
+get_result_set_df <- function(root_path, folder_type, target_columns_df) {
+  resultFileName <- paste("X_", folder_type, ".txt", sep="")
+  resultDF <- fread(paste(root_path, folder_type, resultFileName, sep = "/"))
+  resultDF <- resultDF[, target_columns_df$index, with = FALSE]
+  names(resultDF) <- target_columns_df$measure
+  resultDF
+}
+
+#' This function retreives the column header data for the result set from the 
+#' parent folder and returns a formatted subset as a data.table
+#' 
+#' @param root_path The root path under which the train and test folders are located
+#' to "data"
+#' @examples
+#' > get_selected_column_names_and_indexes_df("/path/to/data/folder")
+#' @return Formatted column names and indexes of only standard deviation and mean 
+#' columns
+#' @export
+get_selected_column_names_and_indexes_df <- function(root_path) {
+  resultSetcolumn_names <- fread(paste(root_path, "features.txt", sep="/"))
+  names(resultSetcolumn_names) <- c("index", "meas")
+  target_columns_df <- resultSetcolumn_names[grep("([Mm]ean|std)", resultSetcolumn_names$meas)]
+  target_columns_df <- mutate(target_columns_df, measure=rename_column(meas))
+  target_columns_df$meas = NULL
+  target_columns_df
+}
+
+#' This function alters a string value to a more human readable and appropriate
+#' form for a header value. It alters the name to include:
+#' - Changes "f" abbreviation to "frequ" as a more readable abbreviation for frequency measurement
+#' - Changes "t" abbreviation to "timeof" as a more readable abbreviation for time of measurement
+#' - Changes "acc" abbreviation to "accel" as a more readable abbreviation for acceleration
+#' - Changes "mag" abbreviation to "magn" as a more readable abbreviation for magnitude
+#' - Changes "mag" abbreviation to "magn" as a more readable abbreviation for magnitude
+#' - Formats characters not appropriate for headers, such as parentheses and commas 
+#' 
+#' @param column_name String value to modify
+#' @examples
+#' > rename_column("tBodyAcc-mean()-Y")
+#' @return Formatted column name
+#' @export
+rename_column <- function(column_name) {
+  returncolumn_name <- gsub("([A-Z])", "_\\1", column_name)
+  returncolumn_name <- tolower(returncolumn_name)
+  returncolumn_name <- gsub("^f", "frequ_", returncolumn_name)
+  returncolumn_name <- gsub("^t", "timeof_", returncolumn_name)
+  returncolumn_name <- gsub("\\(", "_", returncolumn_name)
+  returncolumn_name <- gsub("\\)", "_", returncolumn_name)
+  returncolumn_name <- gsub("-", "_", returncolumn_name)
+  returncolumn_name <- gsub(",", "_", returncolumn_name)
+  returncolumn_name <- gsub("_t_", "_timeof_", returncolumn_name)
+  returncolumn_name <- gsub("acc", "_accel", returncolumn_name)
+  returncolumn_name <- gsub("mag", "_magn", returncolumn_name)
+  returncolumn_name <- gsub("gravity", "_gravity", returncolumn_name)
+  returncolumn_name <- gsub("mean", "_mean", returncolumn_name)
+  returncolumn_name <- gsub("_+", "_", returncolumn_name)
+  returncolumn_name <- gsub("_+$", "", returncolumn_name)
+  returncolumn_name
+}
